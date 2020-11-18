f4e52a98c1ca38cb77f4d877d7b1cecd2f975bc7
One1-Piece <69095588+One1-Piece@users.noreply.github.com>
2020-11-18 14:04:01 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+
+
+function App() {
+  return (
+    <div className="App">
+    {/* NavBar */}
+    <Nav />
+
+    {/* Banner */}
+    <Banner />
+     <Row 
+     title="NETFLIX ORIGINALS"  
+     fetchUrl={requests.fetchNetflixOriginals} 
+     isLargeRow
+     />
+     <Row title= "TRENDING NOW" fetchUrl={requests.fetchTrending} />
+     <Row title= "TOP RATED" fetchUrl={requests.fetchTopRated} />
+     <Row title= "ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+     <Row title= "COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+     <Row title= "HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+     <Row title= "ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies} />
+     <Row title= "DRAMA" fetchUrl={requests.fetchDrama} />
+     <Row title= "DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/requests.js
@@ -0,0 +1,15 @@
+const API_KEY = "d7d1968b629172bbcfcee5dd639e7546";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals:`/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchDrama: `/discover/movie?api_key=${API_KEY}&with_genres=18`,
+};
+
+export default requests;
